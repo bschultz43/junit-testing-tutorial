cd09b3d9709c9b9aa0b8e2ed3b01360616e66bf8
Brittney Schultz <bschultz43@wctc.edu>
2024-08-08 10:17:25 -0500

    Initial commit

---
+++ src/test/java/BasicTests.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BasicTests {
+    @Test
+    public void compare() {
+        Basics basicTests = new Basics();
+        int value = basicTests.compare(2, 1);
+
+        Assertions.assertEquals(1, value);
+    }
+
+    @Test
+    @DisplayName("First number is less than the second")
+    public void compare2() {
+        Basics basicTests = new Basics();
+        int value = basicTests.compare(2, 3);
+        Assertions.assertEquals(-1, value);
+    }
+
+    @Test
+    @DisplayName("First number is equal to the second")
+    public void compare3() {
+        Basics basicTests = new Basics();
+        int value = basicTests.compare(2, 2);
+        Assertions.assertEquals(0, value);
+    }
+
+    @Test
+    @DisplayName("Array sorted")
+    public void sortArray() {
+        Basics basicTests = new Basics();
+        int[] array = {5, 8, 3, 9, 1, 6};
+        basicTests.sortArray(array);
+        Assertions.assertArrayEquals(new int[]{1, 3, 5, 6, 8, 9}, array);
+    }
+
+    @Test
+    @DisplayName("The string has been reversed")
+    public void reverseString() {
+        Basics basicTests = new Basics();
+        String word = "code";
+        String reverse = basicTests.reverseString(word);
+        Assertions.assertEquals(reverse, "edoc");
+    }
+}
